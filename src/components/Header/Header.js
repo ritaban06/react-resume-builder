8f30a2f686943fcdf1a131efa4c69440ac9fd524
ritaban06 <ghoshritabancollege@gmail.com>
2024-09-22 10:15:58 +0530

    Removed svg

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import resumeSvg from "../../assets/resume.svg"
+// import resumeSvg from "../../assets/resume.svg"
 import styles from "./Header.module.css"
 function Header() {
   return (
